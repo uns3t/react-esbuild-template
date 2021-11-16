795ee64ef28a074ef3006b22abce98439c82a6e2
uns3t <zhangshiyu.0@bytedance.com>
2021-11-16 17:58:46 +0800

    feat: add less loader

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,5 +1,33 @@
 const esbuild = require('esbuild');
+const { lessLoader } = require('esbuild-plugin-less');
 const { htmlPlugin } = require('@craftamap/esbuild-plugin-html');
+
+const htmlPluginOptions = {
+    files: [
+        {
+            entryPoints: [
+                'src/app.jsx',
+            ],
+            filename: 'index.html',
+            htmlTemplate: `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>React-esbuild-template</title>
+    </head>
+    <body>
+        <div id="root">
+        </div>
+    </body>
+    </html>
+  `,
+        },
+    ]
+}
+
+console.log(lessLoader);
 
 const options = {
     entryPoints: ['src/app.jsx'],
@@ -7,31 +35,12 @@ const options = {
     metafile: true, // needs to be set
     outdir: 'dist/', // needs to be set
     plugins: [
-        htmlPlugin({
-            files: [
-                {
-                    entryPoints: [
-                        'src/app.jsx',
-                    ],
-                    filename: 'index.html',
-                    htmlTemplate: `
-            <!DOCTYPE html>
-            <html lang="en">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>React-esbuild-template</title>
-            </head>
-            <body>
-                <div id="root">
-                </div>
-            </body>
-            </html>
-          `,
-                },
-            ]
-        })
-    ]
+        htmlPlugin(htmlPluginOptions),
+        lessLoader()
+    ],
+    loader: {
+        '.ts': 'ts',
+    },
 }
 
 esbuild.build(options).catch(() => process.exit(1))
--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './style.css';
+import './style.less';
 
 const root = document.createElement('div');
 root.className = 'root';
